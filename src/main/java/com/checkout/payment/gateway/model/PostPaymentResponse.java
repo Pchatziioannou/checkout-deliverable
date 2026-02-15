532d7963b4bb3d8d9d1717f8369a8d8c46bfe88d
pchatziio <pchatziio@gmail.com>
2026-02-15 12:55:08 +0000

    Modify payment flow (controller, service, model)

--- src/main/java/com/checkout/payment/gateway/model/PostPaymentResponse.java
+++ src/main/java/com/checkout/payment/gateway/model/PostPaymentResponse.java
@@ -1,73 +1,21 @@
 package com.checkout.payment.gateway.model;
 
 import com.checkout.payment.gateway.enums.PaymentStatus;
+import lombok.Builder;
+import lombok.Data;
 import java.util.UUID;
 
+@Data
+@Builder(setterPrefix = "set")
 public class PostPaymentResponse {
   private UUID id;
+  private String authorizationCode;
   private PaymentStatus status;
   private int cardNumberLastFour;
   private int expiryMonth;
   private int expiryYear;
   private String currency;
   private int amount;
-
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public PaymentStatus getStatus() {
-    return status;
-  }
-
-  public void setStatus(PaymentStatus status) {
-    this.status = status;
-  }
-
-  public int getCardNumberLastFour() {
-    return cardNumberLastFour;
-  }
-
-  public void setCardNumberLastFour(int cardNumberLastFour) {
-    this.cardNumberLastFour = cardNumberLastFour;
-  }
-
-  public int getExpiryMonth() {
-    return expiryMonth;
-  }
-
-  public void setExpiryMonth(int expiryMonth) {
-    this.expiryMonth = expiryMonth;
-  }
-
-  public int getExpiryYear() {
-    return expiryYear;
-  }
-
-  public void setExpiryYear(int expiryYear) {
-    this.expiryYear = expiryYear;
-  }
-
-  public String getCurrency() {
-    return currency;
-  }
-
-  public void setCurrency(String currency) {
-    this.currency = currency;
-  }
-
-  public int getAmount() {
-    return amount;
-  }
-
-  public void setAmount(int amount) {
-    this.amount = amount;
-  }
 
   @Override
   public String toString() {
--- src/main/java/com/checkout/payment/gateway/service/PaymentGatewayService.java
+++ src/main/java/com/checkout/payment/gateway/service/PaymentGatewayService.java
@@ -1,6 +1,9 @@
 package com.checkout.payment.gateway.service;
 
+import com.checkout.payment.gateway.client.BankClient;
+import com.checkout.payment.gateway.enums.PaymentStatus;
 import com.checkout.payment.gateway.exception.EventProcessingException;
+import com.checkout.payment.gateway.model.BankResponse;
 import com.checkout.payment.gateway.model.PostPaymentRequest;
 import com.checkout.payment.gateway.model.PostPaymentResponse;
 import com.checkout.payment.gateway.repository.PaymentsRepository;
@@ -9,15 +12,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import static com.checkout.payment.gateway.validator.PaymentValidator.validatePayment;
+
 @Service
 public class PaymentGatewayService {
 
   private static final Logger LOG = LoggerFactory.getLogger(PaymentGatewayService.class);
 
   private final PaymentsRepository paymentsRepository;
+  private final BankClient bankClient;
 
-  public PaymentGatewayService(PaymentsRepository paymentsRepository) {
+  public PaymentGatewayService(PaymentsRepository paymentsRepository, BankClient bankClient) {
     this.paymentsRepository = paymentsRepository;
+    this.bankClient = bankClient;
   }
 
   public PostPaymentResponse getPaymentById(UUID id) {
@@ -25,7 +32,37 @@ public class PaymentGatewayService {
     return paymentsRepository.get(id).orElseThrow(() -> new EventProcessingException("Invalid ID"));
   }
 
-  public UUID processPayment(PostPaymentRequest paymentRequest) {
-    return UUID.randomUUID();
+  public PostPaymentResponse processPayment(PostPaymentRequest paymentRequest) {
+    try {
+      validatePayment(paymentRequest);
+      BankResponse bankResponse = bankClient.authorize(paymentRequest);
+
+      PostPaymentResponse paymentResponse = PostPaymentResponse.builder()
+          .setId(UUID.randomUUID())
+          .setAuthorizationCode(bankResponse.getAuthorizationCode())
+          .setStatus(
+              bankResponse.isAuthorized() ? PaymentStatus.AUTHORIZED : PaymentStatus.DECLINED)
+          .setCardNumberLastFour(Integer.parseInt(paymentRequest.getCardNumberLastFour()))
+          .setExpiryMonth(paymentRequest.getExpiryMonth())
+          .setExpiryYear(paymentRequest.getExpiryYear())
+          .setCurrency(paymentRequest.getCurrency())
+          .setAmount(paymentRequest.getAmount())
+          .build();
+
+      paymentsRepository.add(paymentResponse);
+      return paymentResponse;
+    } catch (EventProcessingException e) {
+      return PostPaymentResponse.builder()
+          .setId(UUID.randomUUID())
+          .setAuthorizationCode("NA")
+          .setStatus(PaymentStatus.REJECTED)
+          .setCardNumberLastFour(Integer.parseInt(paymentRequest.getCardNumberLastFour()))
+          .setExpiryMonth(paymentRequest.getExpiryMonth())
+          .setExpiryYear(paymentRequest.getExpiryYear())
+          .setCurrency(paymentRequest.getCurrency())
+          .setAmount(paymentRequest.getAmount())
+          .build();
+    }
   }
 }
+
